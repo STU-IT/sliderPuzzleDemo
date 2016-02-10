7916d5f2c1a6d86216b8b7eff8f33bc3bab3b573
Søren Magnusson <soren@magnussons.dk>
2016-02-10 19:05:50 +0100

    Nu med dynamisk opbygning af spilleplades markup

--- interaktion.js
+++ interaktion.js
@@ -26,28 +26,53 @@ for (var i = 0;  i < cells.length; i++)
     }, false);
 }
 
+function clickOnBrick( event )
+{
+    var x = this.parentNode.rowIndex;
+    var y = this.cellIndex;
+    if (game.canBrickMove(x, y))
+    {
+        game.moveBrick(x, y);
+        showBoard(game)
+    }
+}
+
 function showBoard ( game )
 {
+    for (var i = gamepad.rows.length-1; i >= 0; i--)
+    {
+        gamepad.deleteRow(i);
+    }
     for (var i = 0; i < game.board.length; i++)
     {
-        var tr = gamepad.firstElementChild.children[i];
+        //var tr = gamepad.firstElementChild.children[i];
+        var tr = document.createElement("tr");
+        gamepad.appendChild(tr);
+
         var collumn = game.board[i];
         for (var j = 0; j < collumn.length; j++)
         {
-            var td = tr.children[j];
-            var span = td.firstElementChild;
+            var td = document.createElement("td");
+            td.classList.add("cell");
+            tr.appendChild(td);
+
+            var span = document.createElement("span");
+            td.appendChild(span);
+
             if (game.board[i][j])
             {
                 span.innerHTML = game.board[i][j].value;
+                td.addEventListener("click", clickOnBrick, false);
             }
             else
             {
                 td.innerHTML = "";
+                td.class = "";
             }
 
         }
     }
 }
 
-var game = new Game(2);
+var game = new Game(3);
 showBoard(game);
